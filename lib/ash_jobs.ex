f73dc6cb6e92bec6f01ef0225e68ef2e06ddc3d6
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-10-13 15:14:40 +0200

    inital commit

---
+++ lib/ash_jobs.ex
@@ -0,0 +1,18 @@
+defmodule AshJobs do
+  @moduledoc """
+  Documentation for `AshJobs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AshJobs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ash_jobs_test.exs
@@ -0,0 +1,8 @@
+defmodule AshJobsTest do
+  use ExUnit.Case
+  doctest AshJobs
+
+  test "greets the world" do
+    assert AshJobs.hello() == :world
+  end
+end
